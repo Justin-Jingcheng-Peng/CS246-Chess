b99c3549c02d7db14435c9bc0ca6ff21449af63b
M. S. Christopher S <43445879+mschristophers@users.noreply.github.com>
2022-12-03 06:58:58 +0000

    added stuff in textObserver

--- textObserver.cc
+++ textObserver.cc
@@ -9,7 +9,13 @@ TextObserver::TextObserver(ChessBoard *chessBoard) : chessBoard{chessBoard} {
 }
 
 TextObserver::~TextObserver() {
+  // map<string, int> score = chessBoard->get_score();
+  // cout << "Final Score:" << endl;
+  // for (auto it = score.begin(); it != score.end(); ++it) {
+  //   cout << it->first << ": " << it->second << endl;
+  // }
   
+  chessBoard->detach(this);
 }
 
 TextObserver::notify(string gameResult) {
